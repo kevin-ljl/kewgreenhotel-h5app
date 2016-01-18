77d3934df6a906c62537d74b7c124010682e1a8b
kevin-ljl <983313585@qq.com>
2016-01-18 20:17:24 +0800

    201601182017

---
+++ learn.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Learn</title>
+        <meta charset="UTF-8">
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="viewport" content="width=640, user-scalable=no"/>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+<style>
+    html body{
+        width:100%;
+        margin: 0px;
+    }
+    #bg{
+        padding:0;
+        margin:0;
+        
+    }
+    .button{
+        position: absolute;
+        z-index: 1001;
+        left:202px;
+        top:2558px;
+    }
+
+</style>
+</head>
+<body>
+            <section>
+                 <img  id="bg" src="images/p8/bg8.png" alt="">
+                 <div class="button">
+                    <img src="images/p8/1.png" alt="">
+                 </div>       
+            </section>
+</body>
+</html>
